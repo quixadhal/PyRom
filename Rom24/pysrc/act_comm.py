89415f90ba755d371f78eab45b5d606f0a50b7f3
davion <davionkalhen@gmail.com>
2014-06-23 10:52:58 -0700

    Some odd text got added on merging. dammit davion pull!

--- Rom24/pysrc/act_comm.py
+++ Rom24/pysrc/act_comm.py
@@ -56,11 +56,7 @@ def do_delete(self, argument):
             ch.pcdata.confirm_delete = False
             return
         else:
-<<<<<<< HEAD
             pfile = os.path.join(settings.PLAYER_DIR, ch.name+'.json')
-=======
-            pfile = os.path.join(PLAYER_DIR, ch.name+'.json')
->>>>>>> aed7fc161462cfaddfe91c8a3490424ede33f0b9
             wiznet("$N turns $Mself into line noise.",ch,None,0,0,0)
             fight.stop_fighting(ch,True)
             ch.do_quit("")
