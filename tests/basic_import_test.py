69f0e622463baed70bcb11ff9ee96cb7b29eff9e
Micheal Taylor <bubthegreat@gmail.com>
2021-12-02 00:24:18 -0800

    Added our first pytest test!

---
+++ tests/basic_import_test.py
@@ -0,0 +1,7 @@
+"""Test that you can import the main rom24 function."""
+
+import importlib
+
+def test_import_rom24():
+    """Import the rom24 module to make sure it can resolve things for import."""
+    importlib.import_module("rom24")
